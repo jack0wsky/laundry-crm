0038cab6e51d58e081595bb4db5b432dd1c8b7e8
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2023-12-27 20:41:23 +0100

    improved calculating total turnover

--- src/frontend/views/MainPanel.tsx
+++ src/frontend/views/MainPanel.tsx
@@ -1,58 +1,41 @@
 import { db } from "@/frontend/laundry.db";
 import { useEffect, useState } from "react";
+import { getAmounts } from "@/frontend/utils/get-total-hotel-usage";
+import { Pricing } from "@/shared/supabase";
 import { useActiveMonth } from "@/frontend/components/use-active-month";
 import { format } from "date-fns";
 
-const calculateTurnover = (data: any[]) => {
-  const sum = data.reduce((acc, item) => {
-    return (acc += item.amount * item.price);
-  }, 0);
-
-  return sum.toLocaleString("pl-PL", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-};
-
 export const MainPanel = () => {
   const [items, setItems] = useState<any[]>([]);
-  const [total, setTotal] = useState(0);
   const { activeDate } = useActiveMonth();
-  const getReport = async () => {
-    const yearAndMonth = format(
-      new Date(activeDate.year, activeDate.month),
-      "yyyy-MM",
+
+  const yearAndMonth = format(
+    new Date(activeDate.year, activeDate.month),
+    "yyyy-MM",
+  );
+
+  const fetchData = async () => {
+    const arr: any[] = [];
+    const hotels = await db.getHotels();
+
+    await Promise.all(
+      (hotels || []).map(async (hotel) => {
+        const reports = await db.getReport(hotel.id, yearAndMonth);
+        const pricing = (await db.getPricing(hotel.name)) as Pricing[];
+        arr.push(...getAmounts(reports, pricing));
+      }),
     );
 
-    const allPricings = await db.getAllPricings();
-
-    const allReports = await db.getAllReports(yearAndMonth);
-
-    const mapped = allReports
-      .map((report) => {
-        const item = (allPricings || []).find(
-          (pricing) =>
-            pricing.product.id === report.product.id &&
-            pricing.hotel.id === report.hotel,
-        );
-
-        return {
-          ...report,
-          price: item?.price,
-        };
-      })
-      .filter((item) => item.amount > 0);
-
-    const total = mapped.reduce((acc, item) => {
-      return (acc = acc + item.amount * item.price);
-    }, 0);
-
-    setTotal(total);
+    setItems(arr);
   };
 
   useEffect(() => {
-    getReport();
+    fetchData();
   }, []);
+
+  const total = items.reduce((acc, item) => {
+    return (acc += item.amount * item.price);
+  }, 0);
 
   return (
     <section className="w-full h-screen ml-[300px] px-5 py-4">
@@ -60,11 +43,7 @@ export const MainPanel = () => {
       <div className="text-2xl mt-5">
         <p className="font-medium text-gray-700">Obrót miesięczny:</p>
         <h2 className="font-bold text-6xl mt-2">
-          {total.toLocaleString("pl-PL", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-          })}{" "}
-          zł
+          {total.toLocaleString("pl-PL")} zł
         </h2>
       </div>
     </section>
