5366a7140d474c9cb7b69d071674c5c3ca3d4425
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2024-01-06 17:56:06 +0100

    update tailwind watch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/frontend/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/frontend/views/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
