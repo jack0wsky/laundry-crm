9fc1b4fda1e409fb0199b2321f2a605dee88604f
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2024-06-22 20:34:17 +0200

    revert protection of login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,11 @@
 import { LoginPage } from "@/modules/auth/LoginPage";
 import { createClient } from "@/lib/auth/supabase/server";
 import { redirect } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Zaloguj się | L'aqua",
+};
 
 export default async function Page() {
   const supabase = createClient();
@@ -8,7 +13,7 @@ export default async function Page() {
   const { data, error } = await supabase.auth.getSession();
 
   if (data && !error) {
-    // return redirect("/");
+    return redirect("/");
   }
 
   return <LoginPage />;
